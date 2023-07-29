3f093f91102a03d1cc806fa91da9eab3fd3f5d51
Escalion <escalion86@gmail.com>
2023-07-29 13:06:34 +0700

    init

---
+++ app/layout.js
@@ -0,0 +1,19 @@
+import './globals.css'
+import '../fonts/InterTight.css'
+import '../fonts/Buyan.css'
+// import localFont from 'next/font/local'
+
+// const inter = localFont({ src: '../fonts/InterTight-Regular.ttf' })
+
+export const metadata = {
+  title: 'Иллюзионист Алексей Белинский',
+  description: 'Уникальное шоу на любое мероприятие',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ru">
+      <body>{children}</body>
+    </html>
+  )
+}
