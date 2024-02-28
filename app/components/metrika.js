8f9a0e5f9177b02f734b2d95db17bd6169198365
Escalion <escalion86@gmail.com>
2024-02-28 13:09:16 +0700

    Update metrika

--- app/components/metrika.js
+++ app/components/metrika.js
@@ -17,24 +17,7 @@ const Metrika = () => {
   useEffect(() => {
     ym(38403125, 'hit', window.location.href)
   }, [pathName, searchParams])
-  return (
-    <Script id="yandex-metrika">
-      {`
-        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-        m[i].l=1*new Date();
-        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-        ym(38403125, "init", {
-          defer: true,
-          clickmap:true,
-          trackLinks:true,
-          accurateTrackBounce:true
-        });    
-      `}
-    </Script>
-  )
+  return null
 }
 
 export default Metrika
--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import '../fonts/Montserrat.css'
 
 import { Suspense } from 'react'
 import Metrika from './components/metrika'
+import Script from 'next/script'
 // import Head from 'next/head'
 
 export const metadata = {
@@ -18,9 +19,27 @@ export default function RootLayout({ children }) {
     <html lang="ru" className="scroll-smooth">
       <body>
         {isProduction && (
-          <Suspense>
-            <Metrika />
-          </Suspense>
+          <>
+            <Script id="yandex-metrika" strategy="afterInteractive">
+              {`
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(38403125, "init", {
+          defer: true,
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true
+        });    
+      `}
+            </Script>
+            <Suspense fallback={<></>}>
+              <Metrika />
+            </Suspense>
+          </>
         )}
         {children}
       </body>
