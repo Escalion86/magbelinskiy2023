a2ccff32b55dce755d8ff8a6ea8ac546e7539e8b
Escalion <escalion86@gmail.com>
2024-02-28 12:37:45 +0700

    Update metrika

--- app/layout.js
+++ app/layout.js
@@ -2,12 +2,7 @@ import './globals.css'
 import '../fonts/InterTight.css'
 import '../fonts/Buyan.css'
 import '../fonts/Montserrat.css'
-// import Provider from './providers'
-// import Head from 'next/head'
-// import { RecoilRoot } from 'recoil'
-// import localFont from 'next/font/local'
 
-// const inter = localFont({ src: '../fonts/InterTight-Regular.ttf' })
 import { Suspense } from 'react'
 import Metrika from './components/metrika'
 
@@ -20,12 +15,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ru" className="scroll-smooth">
       <body>
-        {/* <Provider> */}
-        {children}
-        {/* </Provider> */}
         <Suspense>
           <Metrika />
         </Suspense>
+        {children}
       </body>
     </html>
   )
