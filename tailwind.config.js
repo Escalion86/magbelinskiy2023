8d63684ca8a72d82f1699915189a64265ff7de43
Escalion <escalion86@gmail.com>
2024-08-21 00:48:31 +0700

    Cabinet update

--- tailwind.config.js
+++ tailwind.config.js
@@ -260,13 +260,6 @@ module.exports = {
       transitionDuration: {
         400: '400ms',
       },
-      backgroundSize: {
-        'size-200': '200% 200%',
-      },
-      backgroundPosition: {
-        'pos-0': '0% 0%',
-        'pos-100': '100% 100%',
-      },
     },
     screens: {
       mobile: '375px',
